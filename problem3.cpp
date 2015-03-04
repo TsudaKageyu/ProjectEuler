d29b9ee37cc0a5fc3f5b631bf70ad21199997de5
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-04 13:22:23 +0900

    Improve the performance of Eratosthenes' algorithm in Problem 3.
    Around 350 times faster than the previous version!

--- problem3.cpp
+++ problem3.cpp
@@ -11,36 +11,33 @@ namespace problem3
 
         // List the prime numbers no more than sqrt(num).
 
-        const int64_t max_prime = static_cast<int64_t>(std::sqrt(num) + 1);
-        assert(max_prime <= INT_MAX);
-
         std::vector<int> primes;
-        primes.reserve(static_cast<int>(max_prime / 2));
-
-        primes.push_back(2);
-
-        for (int i = 3; i <= max_prime; i += 2)
-            primes.push_back(i);
-
-        std::vector<int> tmp;
-        tmp.reserve(primes.size());
-
-        for (size_t i = 1; i < primes.size() - 2; ++i)
         {
-            if (primes[i] > primes.back() / 2)
-                break;
+            const int max_prime = static_cast<int>(std::sqrt(num) + 1);
+            assert(max_prime <= INT_MAX);
 
-            tmp.resize(i + 1);
-            ::memcpy(tmp.data(), primes.data(), (i + 1) * sizeof(primes.front()));
+            std::vector<int> numbers;
+            numbers.resize(static_cast<size_t>(max_prime + 1));
 
-            for (size_t j = i + 1; j < primes.size() - 1; ++j)
+            for (size_t i = 0; i < numbers.size(); ++i)
+                numbers[i] = static_cast<int>(i);
+
+            for (size_t i = 2; i < numbers.size() / 2; ++i)
             {
-                if (primes[j] % primes[i] != 0)
-                    tmp.push_back(primes[j]);
+                if (numbers[i] == 0)
+                    continue;
+
+                for (size_t j = i * 2; j < numbers.size(); j += i)
+                    numbers[j] = 0;
             }
 
-            std::swap(tmp, primes);
-            tmp.clear();
+            primes.reserve(numbers.size());
+
+            std::copy_if(
+                numbers.begin() + 2,
+                numbers.end(),
+                std::back_inserter(primes),
+                [](int x) { return x > 0; });
         }
 
         // Find the largest prime factor of num.
