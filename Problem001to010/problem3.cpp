92e1c67ac63652deba3fb6643821759d864920e1
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-05 11:56:31 +0900

    Fix a wrong premise in Problem 3.

--- Problem001to010/problem3.cpp
+++ Problem001to010/problem3.cpp
@@ -5,24 +5,11 @@ int64_t Problem3()
 {
     const int64_t num = 600851475143;
 
-    // List the prime numbers no more than sqrt(num).
-
     const auto primes = Utils::GetPrimeTable();
 
-    const auto itMin = primes.begin();
-    const auto itMax = std::lower_bound(primes.begin(), primes.end(), std::sqrt(num) + 1);
-
-    // Find the largest prime factor of num.
-
-    int64_t answer = 0;
-
-    for (auto it = itMax - 1; it >= itMin; --it)
+    for (auto it = primes.crbegin(); it != primes.crend(); ++it)
     {
-        if (*it < 0)
-            continue;
-
-        const auto d = std::div(num, static_cast<int64_t>(*it));
-        if (d.rem == 0)
+        if (num % *it == 0)
             return *it;
     }
 
