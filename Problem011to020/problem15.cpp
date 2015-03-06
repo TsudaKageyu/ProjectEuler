7300635878003467844515df9db2081811c0fbee
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-06 15:11:17 +0900

    Improve the performance of Problem 15.

--- Problem011to020/problem15.cpp
+++ Problem011to020/problem15.cpp
@@ -4,10 +4,11 @@
 #include <boost/multiprecision/cpp_int.hpp>
 
 using boost::multiprecision::cpp_int;
+using boost::multiprecision::int256_t;
 
-cpp_int Factorial(int64_t n)
+int256_t Factorial(int256_t n)
 {
-    cpp_int f = 1;
+    int256_t f = 1;
 
     for (int64_t i = 2; i <= n; ++i)
         f *= i;
@@ -19,10 +20,10 @@ int64_t Problem15()
 {
     // Calculate 40 C 20 = 40! / (20! * 20!)
 
-    const cpp_int f40 = Factorial(40);
-    const cpp_int f20 = Factorial(20);
+    const int256_t f40 = Factorial(40);
+    const int256_t f20 = Factorial(20);
 
-    const cpp_int answer = f40 / (f20 * f20);
+    const int256_t answer = f40 / (f20 * f20);
 
     return answer.convert_to<int64_t>();
 }
