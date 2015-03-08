e354487dfda0d0509dff10fcb6d37bdcb862fa78
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-09 03:49:01 +0900

    Simplify Problem 24 a bit.

--- Problem021to030/problem24.cpp
+++ Problem021to030/problem24.cpp
@@ -3,7 +3,7 @@
 
 std::string Problem24()
 {
-    int64_t num = 1000000;
+    int64_t num = 999999;
 
     std::vector<char> digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
@@ -12,9 +12,7 @@ std::string Problem24()
     for (int i = 9; i >= 0; --i)
     {
         const int64_t f = Utils::GetFactorial<int64_t>(i);
-        int64_t d = num / f;
-        if (num % f == 0)
-            d--;
+        const int64_t d = num / f;
 
         answer.push_back(digits[d]);
         digits.erase(digits.begin() + d);
