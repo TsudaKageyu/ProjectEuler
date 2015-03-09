b1269c1120ac7287e3fb29d26f4fb704cf8b5f9e
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-09 20:34:38 +0900

    Fix Problem 32 a bit.

--- Problem031to040/problem31.cpp
+++ Problem031to040/problem31.cpp
@@ -3,30 +3,33 @@
 
 int64_t Problem31()
 {
+    const int64_t amount = 200;
     int64_t answer = 0;
 
-    for (int64_t a = 0; a <= 200 / 200; ++a)
+    const int64_t maxA = amount / 200;
+
+    for (int64_t a = 0; a <= maxA; ++a)
     {
-        const int64_t maxB = 2 - 2 * a;
+        const int64_t maxB = (amount - 200 * a) / 100;
 
         for (int64_t b = 0; b <= maxB; ++b)
         {
-            const int64_t maxC = 4 - 4 * a - 2 * b;
+            const int64_t maxC = (amount - 200 * a - 100 * b) / 50;
 
             for (int64_t c = 0; c <= maxC; ++c)
             {
-                const int64_t maxD = (20 - 20 * a - 10 * b - 5 * c) / 2;
+                const int64_t maxD = (amount - 200 * a - 100 * b - 50 * c) / 20;
 
                 for (int64_t d = 0; d <= maxD; ++d)
                 {
-                    const int64_t maxE = 20 - 20 * a - 10 * b - 5 * c - 2 * d;
+                    const int64_t maxE = (amount - 200 * a - 100 * b - 50 * c - 20 * d) / 10;
 
                     for (int64_t e = 0; e <= maxE; ++e)
                     {
-                        const int64_t maxF = 40 - 40 * a - 20 * b - 10 * c - 4 * d - 2 * e;
+                        const int64_t maxF = (amount - 200 * a - 100 * b - 50 * c - 20 * d - 10 * e) / 5;
 
                         for (int64_t f = 0; f <= maxF; ++f)
-                            answer += ((200 - 200 * a - 100 * b - 50 * c - 20 * d - 10 * e - 5 * f) / 2) + 1;
+                            answer += ((amount - 200 * a - 100 * b - 50 * c - 20 * d - 10 * e - 5 * f) / 2) + 1;
                     }
                 }
             }
