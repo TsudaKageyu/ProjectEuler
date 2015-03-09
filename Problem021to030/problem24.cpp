0af0fed5bf009159f7ec64f8361e39aa4423aca7
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-09 10:20:06 +0900

    Improve the performance of Problem 24 and 25.

--- Problem021to030/problem24.cpp
+++ Problem021to030/problem24.cpp
@@ -3,11 +3,11 @@
 
 std::string Problem24()
 {
-    int64_t num = 999999;
-
     std::vector<char> digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
+    int64_t num = 999999;
     std::string answer;
+    answer.reserve(digits.size());
 
     for (int i = 9; i >= 0; --i)
     {
@@ -15,7 +15,7 @@ std::string Problem24()
         const int64_t d = num / f;
 
         answer.push_back(digits[d]);
-        digits.erase(digits.begin() + d);
+        std::swap(digits[d], digits.back());
 
         num -= (f * d);
     }
--- Problem021to030/problem25.cpp
+++ Problem021to030/problem25.cpp
@@ -2,11 +2,12 @@
 #include "utils.h"
 
 using boost::multiprecision::cpp_int;
-using boost::multiprecision::cpp_dec_float_50;
-using boost::multiprecision::log10;
+using boost::multiprecision::pow;
 
 int64_t Problem25()
 {
+    const cpp_int Threshold = pow(cpp_int(10), 999);
+
     int64_t answer = 3;
 
     std::array<cpp_int, 2> buf = { 1, 1 };
@@ -14,10 +15,10 @@ int64_t Problem25()
     while (true)
     {
         const cpp_int next = buf[0] + buf[1];
-        if (log10(cpp_dec_float_50(next)) >= cpp_dec_float_50(999.0))
+        if (next >= Threshold)
             return answer;
 
-        buf[0] = buf[1];
+        buf[0].swap(buf[1]);
         buf[1] = next;
 
         answer++;
