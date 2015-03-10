60d886ef339102dfa8086994ffe2bf0a1b37f8dc
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-10 09:16:07 +0900

    Reorganize the code of Problem 31.

--- Problem031to040/problem31.cpp
+++ Problem031to040/problem31.cpp
@@ -3,33 +3,45 @@
 
 int64_t Problem31()
 {
-    const int64_t amount = 200;
+    const std::array<int64_t, 7> Coins = { 200, 100, 50, 20, 10, 5, 2 };
+
     int64_t answer = 0;
 
-    const int64_t maxA = amount / 200;
+    const int64_t Amount = 200;
+    const int64_t maxA = Amount / Coins[0];
 
     for (int64_t a = 0; a <= maxA; ++a)
     {
-        const int64_t maxB = (amount - 200 * a) / 100;
+        const int64_t restA = Amount - Coins[0] * a;
+        const int64_t maxB  = restA / Coins[1];
 
         for (int64_t b = 0; b <= maxB; ++b)
         {
-            const int64_t maxC = (amount - 200 * a - 100 * b) / 50;
+            const int64_t restB = restA - Coins[1] * b;
+            const int64_t maxC  = restB / Coins[2];
 
             for (int64_t c = 0; c <= maxC; ++c)
             {
-                const int64_t maxD = (amount - 200 * a - 100 * b - 50 * c) / 20;
+                const int64_t restC = restB - Coins[2] * c;
+                const int64_t maxD  = restC / Coins[3];
 
                 for (int64_t d = 0; d <= maxD; ++d)
                 {
-                    const int64_t maxE = (amount - 200 * a - 100 * b - 50 * c - 20 * d) / 10;
+                    const int64_t restD = restC - Coins[3] * d;
+                    const int64_t maxE  = restD / Coins[4];
 
                     for (int64_t e = 0; e <= maxE; ++e)
                     {
-                        const int64_t maxF = (amount - 200 * a - 100 * b - 50 * c - 20 * d - 10 * e) / 5;
+                        const int64_t restE = restD - Coins[4] * e;
+                        const int64_t maxF  = restE / Coins[5];
 
                         for (int64_t f = 0; f <= maxF; ++f)
-                            answer += ((amount - 200 * a - 100 * b - 50 * c - 20 * d - 10 * e - 5 * f) / 2) + 1;
+                        {
+                            const int64_t restF = restE - Coins[5] * f;
+                            const int64_t maxG  = restF / Coins[6];
+
+                            answer += maxG + 1;
+                        }
                     }
                 }
             }
