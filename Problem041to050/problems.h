52ea38a0d9872e86b1afa9b252c894a02ced9137
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-13 21:36:38 +0900

    Solve Problem 44.

--- Problem041to050/problems.h
+++ Problem041to050/problems.h
@@ -3,4 +3,5 @@
 int64_t Problem41();
 int64_t Problem42();
 int64_t Problem43();
+int64_t Problem44();
 int64_t Problem45();
--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main()
     std::cout << table.size() << " prime numbers." << std::endl;
     std::cout << std::endl;
 
-    Utils::Solve(43, Problem43);
+    Utils::Solve(44, Problem44);
     std::cout << std::endl;
 
     Utils::Solve( 1, Problem1);
@@ -67,6 +67,7 @@ int main()
 
     Utils::Solve(41, Problem41);
     Utils::Solve(42, Problem42);
+    Utils::Solve(43, Problem43);
     Utils::Solve(45, Problem45);
 
     Utils::Solve(67, Problem67);
