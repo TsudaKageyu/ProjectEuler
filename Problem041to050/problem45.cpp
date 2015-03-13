e5eb4d998d39f9e7b72d7559023eceea444ce3c4
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-13 11:46:26 +0900

    Improve the performance of Problem 45 by skipping triangular numbers.

--- Problem041to050/problem45.cpp
+++ Problem041to050/problem45.cpp
@@ -16,26 +16,17 @@ namespace
 
         return IsIntegral(tmp / 3.0);
     }
-
-    bool IsHexagonal(int64_t x)
-    {
-        const double tmp = (std::sqrt(2.0 * x + 0.25) + 0.5);
-        if (!IsIntegral(tmp))
-            return false;
-
-        return IsIntegral(tmp / 2.0);
-    }
 }
 
 int64_t Problem45()
 {
-    int64_t t = 40755; // 285th triangular number.
+    int64_t t = 40755; // 143th hexagonal number.
 
-    for (int64_t n = 286; ; ++n)
+    for (int64_t n = 144; ; ++n)
     {
-        t += n;
+        t += n * 4 - 3;
 
-        if (IsPentagonal(t) && IsHexagonal(t))
+        if (IsPentagonal(t))
             return t;
     }
 }
