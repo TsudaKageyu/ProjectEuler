eb77051650789c2d86ffc01b0f2ea5002783fb45
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-16 10:18:52 +0900

    Improve Problem 53.

--- Problem051to060/problem53.cpp
+++ Problem051to060/problem53.cpp
@@ -5,21 +5,24 @@ using boost::multiprecision::cpp_int;
 
 int64_t Problem53()
 {
-    int64_t answer = 0;
+    cpp_int answer = 0;
 
     for (cpp_int n = 23; n <= 100; ++n)
     {
         cpp_int num = 1;
         cpp_int den = 1;
 
-        for (cpp_int r = 1; r <= n; ++r)
+        for (cpp_int r = 1; r <= (n + 1) / 2; ++r)
         {
             num *= (n - r + 1);
             den *= r;
             if (num / den > cpp_int(1000000))
-                answer++;
+            {
+                answer += (n - r * 2 + 1);
+                break;
+            }
         }
     }
 
-    return answer;
+    return static_cast<int64_t>(answer);
 }
