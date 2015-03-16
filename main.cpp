964a269c4967d88b84faac6c40610ce784822b8e
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-03-16 17:59:00 +0900

    Solve Problem 55.

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int wmain(int argc, wchar_t **argv)
     std::cout << table.size() << " prime numbers." << std::endl;
     std::cout << std::endl;
 
-    Utils::Solve(54, Problem54);
+    Utils::Solve(55, Problem55);
     std::cout << std::endl;
 
     if (mode == Latest)
@@ -57,6 +57,7 @@ int wmain(int argc, wchar_t **argv)
     Utils::Solve(51, Problem51);
     Utils::Solve(52, Problem52);
     Utils::Solve(53, Problem53);
+    Utils::Solve(54, Problem54);
     Utils::Solve(59, Problem59);
 
     Utils::Solve(67, Problem67);
