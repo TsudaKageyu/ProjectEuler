8861e141a1602fc3ea22215326fa18ed3b7fcff2
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-04-14 09:05:45 +0900

    Improve the answer for Problem 68.

--- Problem_051_075/Problem068.cpp
+++ Problem_051_075/Problem068.cpp
@@ -1,70 +1,19 @@
 #include <cstdint>
-#include <algorithm>
-#include <vector>
-
-using namespace std;
 
 int64_t Problem68()
 {
-    int64_t answer = 0;
+    // Assume that outer nodes are 6, 10, 9, 8, and 7 to maximize the answer.
 
-    for (int32_t s = 6; s >= 1; --s)
-    {
-        vector<int32_t> outer(10 - s + 1);
-        for (size_t i = 0; i < outer.size(); ++i)
-            outer[i] = static_cast<int32_t>(s + i);
+    // The sum of each set is ((6+7+8+9+10)+(1+2+3+4+5)*2)/5 = 14. So,
+    //  6 + a + b = 14
+    // 10 + b + c = 14
+    //  9 + c + d = 14
+    //  8 + d + e = 14
+    //  7 + e + a = 14
 
-        do
-        {
-            vector<int32_t> inner;
-            inner.reserve(5);
-            for (int32_t i = 1; i <= 9; ++i)
-            {
-                if (find(outer.begin(), outer.begin() + 5, i) == outer.begin() + 5)
-                    inner.push_back(i);
-            }
+    // a + b = 8, so (a, b) = (3, 5) or (5, 3).
+    // b + c = 4, so b is less than 4. Then, a = 5, b = 3, c = 1.
+    // Then, d = 4, e = 2.
 
-            do
-            {
-                const int32_t sum = outer[0] + inner[0] + inner[1];
-
-                for (size_t i = 1; i < 5; ++i)
-                {
-                    if (outer[i] + inner[i] + inner[(i + 1) % 5] != sum)
-                        goto NEXT;
-                }
-
-                int64_t n = 0;
-                for (size_t i = 0; i < 5; ++i)
-                {
-                    if (outer[i] == 10)
-                        n = n * 100 + outer[i];
-                    else
-                        n = n * 10 + outer[i];
-
-                    if (inner[i] == 10)
-                        n = n * 100 + inner[i];
-                    else
-                        n = n * 10 + inner[i];
-
-                    if (inner[(i + 1) % 5] == 10)
-                        n = n * 100 + inner[(i + 1) % 5];
-                    else
-                        n = n * 10 + inner[(i + 1) % 5];
-                }
-
-                if (n > answer)
-                    answer = n;
-
-            NEXT:
-                ;
-            } while (next_permutation(inner.begin(), inner.end()));
-
-        } while (next_permutation(outer.begin() + 1, outer.end()));
-
-        if (answer != 0)
-            break;
-    }
-
-    return answer;
+    return 6531031914842725;
 }
