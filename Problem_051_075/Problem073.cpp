9e800779233438e748e14fff0fbb325243251885
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-04-14 14:23:13 +0900

    Solve Problem 73.

---
+++ Problem_051_075/Problem073.cpp
@@ -0,0 +1,35 @@
+#include <cstdint>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+namespace
+{
+    int64_t gcd(int64_t a, int64_t b)
+    {
+        while (a % b != 0)
+        {
+            a = a % b;
+            swap(a, b);
+        }
+
+        return b;
+    }
+}
+
+int64_t Problem73()
+{
+    int32_t answer = 0;
+
+    for (int32_t d = 4; d <= 12000; ++d)
+    {
+        for (int32_t n = d / 3 + 1; n <= (d - 1) / 2; ++n)
+        {
+            if (gcd(d, n) == 1)
+                answer++;
+        }
+    }
+
+    return answer;
+}
