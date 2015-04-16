578ebc8430f068483e206d0d22e9888bb91addd8
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-04-16 09:43:14 +0900

    Improve the performance of Problem 39.

--- Problem_026_050/Problem039.cpp
+++ Problem_026_050/Problem039.cpp
@@ -1,36 +1,34 @@
 #include <cstdint>
 #include <algorithm>
+#include <array>
+#include <vector>
 
 using namespace std;
 
 int64_t Problem39()
 {
-    int32_t answer   = 0;
-    int32_t maxCount = 0;
+    const size_t Limit = 1000;
 
-    for (int32_t p = 3; p <= 1000; ++p)
+    vector<array<int32_t, 3>> triplets;
+
+    for (int32_t i = 1; i <= 50; ++i)
     {
-        int32_t count = 0;
-
-        for (int32_t a = 1; a < p / 2; ++a)
+        for (int32_t j = 1 + (i & 1); j < i; j += 2)
         {
-            const double b = ((2.0 * a * p) - (p * p)) / (2.0 * (a - p));
-            if (b != floor(b))
-                continue;
-
-            const int32_t c = p - a - static_cast<int32_t>(b);
-            if (c < 1)
-                continue;
-
-            count++;
-        }
-
-        if (count > maxCount)
-        {
-            answer   = p;
-            maxCount = count;
+            const array<int32_t, 3> t = { i * i - j * j, 2 * i * j, i * i + j * j };
+            triplets.push_back(t);
         }
     }
 
-    return answer;
+    vector<int32_t> counts(Limit + 1);
+
+    for (const auto &t : triplets)
+    {
+        const int32_t p = t[0] + t[1] + t[2];
+
+        for (int32_t i = p; i <= Limit; i += p)
+            counts[i]++;
+    }
+
+    return (max_element(counts.begin(), counts.end()) - counts.begin());
 }
