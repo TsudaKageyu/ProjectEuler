1781c6dd4786ac61b836ba8a988147db2a240882
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-04-16 09:52:48 +0900

    Improve the performance of Problem 39.

--- Problem_026_050/Problem039.cpp
+++ Problem_026_050/Problem039.cpp
@@ -1,6 +1,5 @@
 #include <cstdint>
 #include <algorithm>
-#include <array>
 #include <vector>
 
 using namespace std;
@@ -9,23 +8,18 @@ int64_t Problem39()
 {
     const size_t Limit = 1000;
 
-    vector<array<int32_t, 3>> triplets;
+    vector<int32_t> perimeters;
 
-    for (int32_t i = 1; i <= 50; ++i)
+    for (int32_t i = 1; i <= 30; ++i)
     {
         for (int32_t j = 1 + (i & 1); j < i; j += 2)
-        {
-            const array<int32_t, 3> t = { i * i - j * j, 2 * i * j, i * i + j * j };
-            triplets.push_back(t);
-        }
+            perimeters.push_back((i * i - j * j) + (2 * i * j) + (i * i + j * j));
     }
 
     vector<int32_t> counts(Limit + 1);
 
-    for (const auto &t : triplets)
+    for (const auto &p : perimeters)
     {
-        const int32_t p = t[0] + t[1] + t[2];
-
         for (int32_t i = p; i <= Limit; i += p)
             counts[i]++;
     }
