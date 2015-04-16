dd44fc3d964afa200521b7fa6d0de5caa91affd9
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-04-16 11:56:44 +0900

    Improve the performance of Problem 39.

--- Problem_026_050/Problem039.cpp
+++ Problem_026_050/Problem039.cpp
@@ -6,23 +6,22 @@ using namespace std;
 
 int64_t Problem39()
 {
-    const size_t Limit = 1000;
-    const double MaxI  = sqrt(Limit / 2.0 - 0.25) - 0.5;
-
-    vector<int32_t> perimeters;
-
-    for (int32_t i = 1; i < MaxI; ++i)
-    {
-        for (int32_t j = 1 + (i & 1); j < i; j += 2)
-            perimeters.push_back(2 * (i * i + i * j));
-    }
+    const size_t  Limit = 1000;
+    const int32_t MaxI  = static_cast<int32_t>(sqrt(Limit / 2.0 - 0.25) - 0.5);
 
     vector<int32_t> counts(Limit + 1);
 
-    for (const auto &p : perimeters)
+    for (int32_t i = 1; i <= MaxI; ++i)
     {
-        for (int32_t i = p; i <= Limit; i += p)
-            counts[i]++;
+        for (int32_t j = 1 + (i & 1); j < i; j += 2)
+        {
+            const int32_t p = 2 * (i * i + i * j);
+            if (p > Limit)
+                break;
+
+            for (int32_t k = p; k <= Limit; k += p)
+                counts[k]++;
+        }
     }
 
     return (max_element(counts.begin(), counts.end()) - counts.begin());
