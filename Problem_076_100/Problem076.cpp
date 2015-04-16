81303a6115ccd002c583221bce8cbd56498557f6
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-04-16 13:59:04 +0900

    Solve Problem 76.

---
+++ Problem_076_100/Problem076.cpp
@@ -0,0 +1,22 @@
+#include <cstdint>
+#include <vector>
+
+using namespace std;
+
+int64_t Problem76()
+{
+    const int32_t Limit = 100;
+
+    vector<int32_t> p(Limit + 1);
+    p[0] = 1;
+
+    for (size_t i = 1; i <= Limit - 1; ++i)
+    {
+        for (size_t j = i; j <= Limit; ++j)
+        {
+            p[j] += p[j - i];
+        }
+    }
+
+    return 0;
+}
